b58d075297abe5e44fa59c63150e08cc51f6a297
231theo <theo@Xubuntu.myguest.virtualbox.org>
2025-05-14 07:31:58 +0000

    Computer's random choice
    feat(game): add getComputerChoice function for random selection
    Implements logic to randomly return "rock", "paper", or "scissors" using Math.random().

---
+++ script.js
@@ -0,0 +1,12 @@
+//Function to get computer's random choice
+function getComputerChoice() {
+    const randomNumber = Math.random();
+    if(randomNumber < 0.33) {
+        return "rock"
+    } else if(randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors"
+    }
+}
+
